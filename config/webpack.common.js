f2f83f948ae795f6c69f68296625ee011d029557
Storm Heg <storm@stormheg.co>
2019-07-27 10:07:50 +0200

    Fix grid breakpoints and update dependencies

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,68 +1,66 @@
-const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
-    app: './src/index.js',
+    app: "./src/index.js"
   },
   output: {
-    path: path.resolve(process.cwd(), 'dist/'),
-    filename: '[name].js',
+    path: path.resolve(process.cwd(), "dist/"),
+    filename: "[name].js"
   },
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: [
-        'babel-loader',
-      ],
-    }, {
-      test: /\.scss$/,
-      use: [
-        MiniCssExtractPlugin.loader,
-        'css-loader',
-        'sass-loader',
-      ],
-    }, {
-      test: /\.(eot|ttf|woff|woff2|svg)$/,
-      use: {
-        loader: 'file-loader',
-        options: {
-          name: 'assets/[name].[ext]',
-        },
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
       },
-    }],
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2|svg)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "assets/[name].[ext]"
+          }
+        }
+      }
+    ]
   },
   optimization: {
     splitChunks: {
       cacheGroups: {
         vendor: {
           test: /[\\/]node_modules[\\/]/,
-          name: 'vendor',
-          chunks: 'all',
-        },
-      },
+          name: "vendor",
+          chunks: "all"
+        }
+      }
     },
     minimizer: [
       new TerserPlugin({
         cache: true,
-        parallel: true,
+        parallel: true
       }),
-      new OptimizeCSSAssetsPlugin({}),
-    ],
+      new OptimizeCSSAssetsPlugin({})
+    ]
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: path.resolve(process.cwd(), 'src/index.html'),
+      template: path.resolve(process.cwd(), "src/index.html")
     }),
     new MiniCssExtractPlugin({
-      filename: '[name].[contenthash].css',
-      chunkFilename: '[id].css',
-    }),
-  ],
+      filename: "[name].[contenthash].css",
+      chunkFilename: "[id].css"
+    })
+  ]
 };
